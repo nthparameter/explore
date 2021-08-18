ed7e85320d5ad1bc3d1cae37f2c653df4b903d63
Dave <nthparameter@gmail.com>
2021-08-17 21:15:28 -0700

    wip. initial https download

---
+++ rust/gcs/src/main.rs
@@ -0,0 +1,19 @@
+
+use hyper_tls::HttpsConnector;
+use hyper::Client;
+
+async fn test_hyper() -> Result<(), Box<dyn std::error::Error>>{
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+
+    let res = client.get("https://hyper.rs".parse()?).await?;
+    assert_eq!(res.status(), 200);
+    Ok(())
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Hello, world!");
+    test_hyper();
+    Ok(())
+}
