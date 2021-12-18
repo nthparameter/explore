ddb6de85fc870a9f6bf67dfc35c4f8a85b8f320c
Dave <nthparameter@gmail.com>
2021-12-18 15:54:54 -0800

    wip

--- rust/gcs/src/main.rs
+++ rust/gcs/src/main.rs
@@ -1,17 +1,31 @@
 
 use hyper_tls::HttpsConnector;
-use hyper::Client;
+use hyper::{body::HttpBody, client::Client};
+use std::io::Write;
+
+struct Well;
+
+impl AsyncReader for Well {
+    
+}
 
 async fn test_hyper() -> Result<(), Box<dyn std::error::Error>>{
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
 
-    let res = client.get("https://hyper.rs".parse()?).await?;
+    let mut res = client.get("https://hyper.rs".parse()?).await?;
     assert_eq!(res.status(), 200);
+    let aa = std::io::stdout();
+    let mut stdout = aa.lock();
+    while let Some(next) = res.data().await {
+        let chunk = next?;
+        stdout.write_all(&chunk)?;
+    }
     Ok(())
 }
 
-#[tokio::main(flavor = "current_thread")]
+//#[tokio::main(flavor = "current_thread")]
+#[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
     test_hyper().await?;
